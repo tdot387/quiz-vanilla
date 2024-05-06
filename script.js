fc9f564ddfbe04aaab7ac5cca1878e4828783050
Thomas <t-wagner@web.de>
2024-05-06 18:55:25 +0200

    Added questions

--- script.js
+++ script.js
@@ -1,22 +1,36 @@
 let categories;
+let questions;
 
 function toggleDialog(open) {
     document.getElementById("open-game-settings").open = open;
 }
 
-function toPlayers() {
-    const categoriesContent = document.getElementById('players-content').classList.remove('d-none');
+function toQuestions() {
+    const categoriesContent = document.getElementById('questions-content').classList.remove('d-none');
     const playersContent = document.getElementById('categories-content').classList.add('d-none');
     categories = document.getElementById('categories').value;
+}
 
+function toPlayers() {
+    const playersContent = document.getElementById('categories-content').classList.add('d-none');
+    questions = document.getElementById('questions-per-category').value;
 }
 
 function buildGame() {
     const cats = document.getElementById('cats');
     cats.innerHTML = '';
+    // Erstellt die Top-Bar mit den Kategorien
     for(let i = 0; i < categories; i++) {
         cats.innerHTML += `
-        <div class="col-${12 / categories} center-shit border-boxes py-3">Cat ${i + 1}</div>
+        <div class="col-${12 / categories} center-shit border-boxes py-3" id="cat-${i}">Cat ${i + 1}</div>
         `
+    }
+    // Erstellt AKTUELL EINE Reihe von Fragen unter Kat. 1
+    for(let i=0; i < questions; i++) {
+        let qDiv = document.createElement('div');
+        qDiv.innerHTML = 'Hallo' + (i+1);
+        qDiv.id = (i+1)
+        cats.appendChild(qDiv)
+
     }
 }
