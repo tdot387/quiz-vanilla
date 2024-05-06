db40a68945cd196cb0d907864553f08716698508
Thomas <t-wagner@web.de>
2024-05-06 18:35:01 +0200

    added build game function

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
+let categories;
+
 function toggleDialog(open) {
     document.getElementById("open-game-settings").open = open;
 }
 
-function next() {
+function toPlayers() {
     const categoriesContent = document.getElementById('players-content').classList.remove('d-none');
     const playersContent = document.getElementById('categories-content').classList.add('d-none');
+    categories = document.getElementById('categories').value;
+
+}
+
+function buildGame() {
+    const cats = document.getElementById('cats');
+    cats.innerHTML = '';
+    for(let i = 0; i < categories; i++) {
+        cats.innerHTML += `
+        <div class="col-${12 / categories} center-shit border-boxes py-3">Cat ${i + 1}</div>
+        `
+    }
 }
