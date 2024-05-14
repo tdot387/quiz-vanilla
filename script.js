6cab3f074bbcb880b310a1ed35308e6cb5aa5a69
Thomas <t-wagner@web.de>
2024-05-14 20:08:28 +0200

    pushed categories into array, created navbar with cats

--- script.js
+++ script.js
@@ -12,52 +12,72 @@ function toggleDialog(open) {
 }
 
 function submitNumberOfCategories() {
-    categoriesNumber.classList.add('d-none');
-    categories = document.getElementById('categories').value;
-    for (let i = 0; i < categories; i++) {
-        
-        categoriesNames.innerHTML += `
-        <input placeholder="Name of Category ${i}" id="categories-${i}"><br />
+    categories = parseInt(document.getElementById('categories').value);
+    if (categories >= 1 && categories <= 7) {
+        for (let i = 0; i < categories; i++) {
+            categoriesNames.innerHTML += `
+        <input placeholder="Name of Category ${i + 1}" id="categories-${i + 1}"><br />
         `;
-        
+
+        }
+        categoriesNumber.classList.add('d-none');
+        categoriesNames.innerHTML += `<button onclick="submitNamesOfCategories()">Next</button>`;
+        categoriesNames.classList.remove('d-none');
+    } else {
+        let errorMsg = document.getElementById('error-message');
+        if (!errorMsg) {
+            errorMsg = document.createElement('p');
+            errorMsg.id = 'error-message';
+            errorMsg.classList.add('text-danger');
+            document.getElementById('categories').value = '';
+            errorMsg.innerText = 'Please enter a valid number (1-7).'
+            categoriesNumber.appendChild(errorMsg);
+        }
     }
-    categoriesNames.innerHTML += `<button onclick="submitNamesOfCategories()">Next</button>`;
-    categoriesNames.classList.remove('d-none');
 }
 
 
 function submitNamesOfCategories() {
     questionArray = [];
-    for(let i = 0; i < categories; i++) {
-        const selector = `#categories-${i}`;
-            const inputs = document.querySelectorAll(selector);
-            inputs.forEach(input => {
-                questionArray.push(input.value);
-            })
+    for (let i = 0; i < categories; i++) {
+        const selector = `#categories-${i+1}`;
+        const inputs = document.querySelectorAll(selector);
+        inputs.forEach(input => {
+            questionArray.push(input.value);
+        })
     }
+    buildGame();
     toggleDialog();
 }
 
 
 
-// function buildGame() {
-//     const cats = document.getElementById('cats');
-//     cats.innerHTML = '';
-//     // Erstellt die Top-Bar mit den Kategorien
-//     for(let i = 0; i < categories; i++) {
-//         cats.innerHTML += `
-//         <div class="col-${12 / categories} center-shit border-boxes py-3" id="cat-${i}">Cat ${i + 1}</div>
-//         `
-//     }
-//     // Erstellt AKTUELL EINE Reihe von Fragen unter Kat. 1
-//     for(let i=0; i < questions; i++) {
-//         let qDiv = document.createElement('div');
-//         qDiv.innerHTML = 'Hallo' + (i+1);
-//         qDiv.id = (i+1)
-//         cats.appendChild(qDiv)
+function buildGame() {
+    const cats = document.getElementById('cats');
+    cats.innerHTML = '';
+    // Erstellt die Top-Bar mit den Kategorien
+    for(let i = 0; i < categories; i++) {
+        cats.innerHTML += `
+        <div class="col-${12 / categories} center-shit border-boxes py-3" id="categories-${i+1}" onclick="editCategory(event)">${questionArray[i]}-></div>
+        `
+    }
+    // Erstellt AKTUELL EINE Reihe von Fragen unter Kat. 1
+    for(let i=0; i < questions; i++) {
+        let qDiv = document.createElement('div');
+        qDiv.innerHTML = 'Hallo' + (i+1);
+        qDiv.id = (i+1)
+        cats.appendChild(qDiv)
 
-//     }
-// }
+    }
+}
+
+function editCategory(event) {
+    const clickedElement = event.target;
+    const dataId = clickedElement.getAttribute('id');
+    const test = document.getElementById(dataId);
+    test.innerText = 'dsngds';
+    
+}
 
 // async function getJSON() {
 //     const response = await fetch('questions.json');
